c8811729cf21117943c54d62eab0df058c7239d9
menghx <mhxdev@gmail.com>
2014-04-18 17:36:28 +0800

    Update mhx_profile.sh

--- mhx_profile.sh
+++ mhx_profile.sh
@@ -2,14 +2,20 @@
 # mhxdev@gmail.com
 
 # java environment
-export JAVA_HOME=/opt/tools/jdk1.7.0_25
+export JAVA_HOME=/opt/sdks/jdk1.7.0_55
 export CLASSPATH=.:$JAVA_HOME/lib/tools.jar:$JAVA_HOME/lib/dt.jar
+export PATH=$JAVA_HOME/bin:$PATH
+
+# tomcat home
+export CATALINA_HOME=/opt/server/apache-tomcat-7.0.53
+export PATH=$CATALINA_HOME/bin:$PATH
 
 # go environment
-export GOROOT=/opt/tools/go
-export GOBIN=$GOROOT/bin
+export GOROOT=/opt/sdks/go
+#export GOBIN=$GOROOT/bin
+#export GOPATH=/home/menghx/gows
+export PATH=$GOROOT/bin:$PATH
 
 # android environment
-export ANDROID_HOME=/opt/tools/android-sdk-linux
-
-export PATH=$JAVA_HOME/bin:$GOBIN:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$PATH
+#export ANDROID_HOME=/opt/tools/android-sdk-linux
+#export PATH=$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$PATH
