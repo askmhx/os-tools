ecbffea7fb45d8b5249c2b0a1fe05a285997b5ab
Crazz <mhxdev@gmail.com>
2018-07-27 10:05:47 +0800

    Create install-new-mac.sh
    install new mac

---
+++ install-new-mac.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo ln -s /Users/Crazz/Software/Tools/maven350 ./maven
+sudo ln -s /Users/Crazz/Software/Tools/apache-tomcat-9.0.10 ./tomcat
+sudo ln -s /Users/Crazz/Software/Tools/gradle-4.9  /usr/local/gradle
