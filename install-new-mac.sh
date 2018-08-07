ad9749c638d7a40512cd11c4a39b824505cb214b
Crazz <mhxdev@gmail.com>
2018-08-07 09:43:26 +0800

    Update install-new-mac.sh

--- install-new-mac.sh
+++ install-new-mac.sh
@@ -2,3 +2,5 @@
 sudo ln -s /Users/Crazz/Software/Tools/maven350 ./maven
 sudo ln -s /Users/Crazz/Software/Tools/apache-tomcat-9.0.10 ./tomcat
 sudo ln -s /Users/Crazz/Software/Tools/gradle-4.9  /usr/local/gradle
+ln -s /Users/Crazz/Software/Tools/maven350/conf/settings.xml ~/.m2/settings.xml
+ln -s /Users/Crazz/Software/Tools/repository ~/.m2/repository
